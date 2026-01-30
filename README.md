20bb997337a0c49807845d6a6744b41844fa4c7b
Kenneth Ouy <kenneth.ouy4@gmail.com>
2026-01-30 17:17:04 -0500

    repo creation

---
+++ README.md
@@ -0,0 +1,2 @@
+My name is Kenneth Ouy. This repo is my first homework submission for CISC3140.
+
---
+++ hw1.js
@@ -0,0 +1,20 @@
+// part 1 
+//sum function
+function sum(arr){
+    let total = 0;
+
+    for(const n of arr){
+        total += num;
+    }
+
+    return total;
+}
+//average function
+function average(arr){
+    return sum(arr) / arr.length;
+}
+
+//mix function
+function min(arr){
+    
+}
